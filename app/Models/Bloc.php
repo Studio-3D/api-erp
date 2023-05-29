a3c6d8c57756f999efc256128060e425b537e8f8
Mohamed CHAABA <88441166+flavio-back-end@users.noreply.github.com>
2023-05-29 19:17:21 +0100

    Update Bloc.php

--- app/Models/Bloc.php
+++ app/Models/Bloc.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Bloc extends Model
 {
     use HasFactory;
+     protected $fillable = [
+        'nom',
+        'projet_id',
+        'tranche_id',
+        'titre_foncier',
+    ];
+    public function projet()
+    {
+        return $this->belongsTo(Projet::class);
+    }
+
+    public function tranche()
+    {
+        return $this->belongsTo(Tranche::class);
+    }
 }
