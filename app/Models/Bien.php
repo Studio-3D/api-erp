ed144108b9641cf1c8b221f6318e87fdfa627649
Mohamed CHAABA <88441166+flavio-back-end@users.noreply.github.com>
2023-05-30 14:39:25 +0100

    Update Bien.php

--- app/Models/Bien.php
+++ app/Models/Bien.php
@@ -35,7 +35,7 @@ class Bien extends Model
     ];
     protected $dates = ['deleted_at'];
 
-    public function TypeBien()
+    public function typebien
     {
         return $this->belongsTo(TypeBien::class, 'type_id');
     }
