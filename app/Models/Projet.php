ededc93a5235d5c99c2f610f2a900f864f7b8782
Mohamed CHAABA <88441166+flavio-back-end@users.noreply.github.com>
2023-05-30 15:08:05 +0100

    Update Projet.php

--- app/Models/Projet.php
+++ app/Models/Projet.php
@@ -9,22 +9,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Projet extends Model
 {
     use HasFactory, SoftDeletes;
-    protected $fillable = [
-        'nom',
-        'code',
-        'adresse',
-        'date_autorisation_construction',
-        'date_permis_habiter',
-        'titre_foncier',
-        'surface_terrain',
-        'prix_acquisition',
-        'limite_annulation_reservation',
-        'type_id',
-        'nbr_tranches',
-        'nbr_blocs',
-        'nbr_immeubles',
-        'nbr_biens'
-    ];
+       protected $table = 'projets';
+
     protected $dates = ['deleted_at'];
     public function typeprojet()
     {
