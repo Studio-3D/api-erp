f82cd01f39029231776bb171f3576fa5ac9d1d3a
Mohamed CHAABA <88441166+flavio-back-end@users.noreply.github.com>
2023-05-30 15:15:45 +0100

    Update 2023_05_23_155655_create_type_projets_table.php

--- database/migrations/2023_05_23_155655_create_type_projets_table.php
+++ database/migrations/2023_05_23_155655_create_type_projets_table.php
@@ -12,9 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('type_projets', function (Blueprint $table) {
+             $table->id();
             $table->string('type');
-            $table->unsignedBigInteger('projet_id');
-            $table->foreign('projet_id')->references('id')->on('projets');
             $table->timestamps();
             $table->softDeletes();
         });
