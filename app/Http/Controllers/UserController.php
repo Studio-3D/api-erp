dd5bd3a652c597b7ba79dc748eb0cd50a7557aa0
Mohamed CHAABA <88441166+flavio-back-end@users.noreply.github.com>
2023-12-19 13:16:16 +0100

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -49,7 +49,7 @@ class UserController extends Controller
             $user->societe_id = 1;
             $user->save();
         }
-         $user->is_connected = 1;
+         $user->is_connected = 0;
             $user->save();
         return response()->json([
             'message' => 'Logout successful',
