f39eed5f2825e0e4fc53ec00827ac570fee2d4d2
unknown <aissiouinabila@gmail.com>
2024-01-31 10:36:51 +0100

    comit1

--- app/Http/Requests/UpdateTrancheRequest.php
+++ app/Http/Requests/UpdateTrancheRequest.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 use App\Http\Helpers\DatabaseHelper;
 use App\Models\Societe;
+use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class UpdateTrancheRequest extends FormRequest
 {
@@ -24,19 +24,19 @@ class UpdateTrancheRequest extends FormRequest
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
      */
     public function rules(): array
-    {   $societe_id = Auth::guard('api')->user()->societe_id;
-        $societe=Societe::findOrfail( $societe_id);
-        $DatabaseName='Erp_'.$societe->raison_sociale.'_'.$societe_id;
+    {$societe_id = Auth::guard('api')->user()->societe_id;
+        $societe = Societe::findOrfail($societe_id);
+        $DatabaseName = 'Erp_' . $societe->raison_sociale . '_' . $societe_id;
         DatabaseHelper::Config();
         return [
             'projet_id' => 'integer',
-            'date_lancement' => 'date',
-            'date_livraison' => 'date',
+            'date_lancement' => 'date|nullable',
+            'date_livraison' => 'date|nullable',
             'niveau_etages' => 'integer',
             'nbre_blocs' => 'integer ',
             'nbre_immeubles' => 'integer',
             'nbre_biens' => 'integer',
-            'nom' => [Rule::unique('temp.'.$DatabaseName.'.tranches','nom')->where(function ($query) {
+            'nom' => [Rule::unique('temp.' . $DatabaseName . '.tranches', 'nom')->where(function ($query) {
                 $query->where('nom', $this->nom)
                     ->where('projet_id', $this->projet_id);})->ignore($this->tranche)],
         ];
