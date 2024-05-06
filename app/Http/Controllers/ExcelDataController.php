0ab469317a4336c6af3b900055b026ebec360887
mohamedchaaba <mohamedchaaba7@gmail.com>
2024-05-06 16:52:32 +0100

    fix probleme import completely

--- app/Http/Controllers/ExcelDataController.php
+++ app/Http/Controllers/ExcelDataController.php
@@ -30,68 +30,30 @@ class ExcelDataController extends Controller{
 
        
     public function UploadDataExcel(Request $request){
-        // Retrieve project ID from the request
+        
         
         $projet_id = $request->projetId;
 
-        // Configure the database
         DatabaseHelper::Config();
 
-        // Retrieve project from the database
         $projet = Projet::on('temp')->findOrFail($projet_id);
 
-        // Set execution limits
         set_time_limit(0);
         ini_set('memory_limit', '-1');
 
-        // Retrieve data from the request
         $data = $request->input('data');
 
-        // Extract keys from the first element of the data array
         $keys = array_keys($data[0]);
         
         Log::info($keys);
         
 
-        // Determine import method based on key existence
         $importMethod = $this->determineImportMethod($keys);
 
-        // Call the appropriate import method and return the result
         return ImportExcelHelper::$importMethod($data, $projet_id);
     }
     
-        // Method to determine the import method based on key existence
-        // private function determineImportMethod($keys) {
-        //     if (in_array('tranche', $keys) && in_array('bloc', $keys) && in_array('immeuble', $keys)) {
-                
-        //         Log::info('ImportStockByProjet');
-                
-        //         return 'ImportStockByProjet';
-        //     } elseif (in_array('tranche', $keys) && in_array('bloc', $keys)) {
-        //         return 'ImportStockByProjetWithoutImmeuble';
-        //     } elseif (in_array('tranche', $keys)) {
-        //         Log::info('ImportStockByProjetWithoutBlocAndImmeuble');
-
-        //         return 'ImportStockByProjetWithoutBlocAndImmeuble';
-        //     } elseif (in_array('bloc', $keys) && in_array('immeuble', $keys)) {
-        //         Log::info('ImportStockByProjetWithoutTranche');
-
-        //         return 'ImportStockByProjetWithoutTranche';
-        //     } elseif (in_array('bloc', $keys)) {
-        //         Log::info('ImportStockByProjetWithoutTrancheAndImmeuble');
-
-        //         return 'ImportStockByProjetWithoutTrancheAndImmeuble';
-        //     } elseif (in_array('immeuble', $keys)) {
-        //         Log::info('ImportStockByProjetWithoutTrancheAndBloc');
-
-
-        //         return 'ImportStockByProjetWithoutTrancheAndBloc';
-        //     } else {
-        //         Log::info('ImportStockByProjetWithoutTrancheAndBlocAndImmeuble');
-
-        //         return 'ImportStockByProjetWithoutTrancheAndBlocAndImmeuble';
-        //     }
-        // }
+        
         private function determineImportMethod($keys) {
             $hasTranche = in_array('tranche', $keys);
             $hasBloc = in_array('Bloc', $keys);
