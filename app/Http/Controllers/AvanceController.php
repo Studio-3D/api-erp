f0d4c61068a885143418d2b49f26871e0ca05b69
FADWA-GHRISSI <47297132+FADWA-GHRISSI@users.noreply.github.com>
2024-05-09 13:47:43 +0100

    Update AvanceController.php

--- app/Http/Controllers/AvanceController.php
+++ app/Http/Controllers/AvanceController.php
@@ -294,7 +294,9 @@ class AvanceController extends Controller
                     $avance->statut = StatutReservationEnum::Validé->value;
                 }
             }
-
+             if($request->montant==0){
+                $avance->statut = StatutReservationEnum::Validé->value;
+            }
             if ($avance->save()) {
                 //store statut_avances table=>si validé
                 if($avance->statut==StatutReservationEnum::Validé->value ){
