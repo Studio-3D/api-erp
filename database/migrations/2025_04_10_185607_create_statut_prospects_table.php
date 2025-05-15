1df7425038215b1c50e4dd874c2a92cf28d2336a
FADWA-GHRISSI <47297132+FADWA-GHRISSI@users.noreply.github.com>
2025-05-15 15:09:50 +0100

    Update 2025_04_10_185607_create_statut_prospects_table.php

--- database/migrations/2025_04_10_185607_create_statut_prospects_table.php
+++ database/migrations/2025_04_10_185607_create_statut_prospects_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('statut_prospects', function (Blueprint $table) {
             $table->id();
             $table->foreignId('prospect_id')->constrained('prospects')->onDelete('cascade');
-            $table->enum('statut',[StatutProspectEnum::Planification_rdv->value,StatutProspectEnum::Injoignable->value,StatutProspectEnum::Rappel->value,StatutProspectEnum::Convert_visite->value,StatutProspectEnum::nb_appel->value])->comment('1=>Planification_rdv 2=>Injoignable 3=>Rappel 4=>visite 5==>store un nv appel');
+            $table->enum('statut',[StatutProspectEnum::Planification_rdv->value,StatutProspectEnum::Injoignable->value,StatutProspectEnum::Rappel->value,StatutProspectEnum::Convert_visite->value,StatutProspectEnum::nv_appel->value])->comment('1=>Planification_rdv 2=>Injoignable 3=>Rappel 4=>visite 5==>store un nv appel');
             $table->foreignId('user_id_traite')->constrained('users')->onDelete('cascade');
             $table->date('date_traitement')->nullable();
             $table->dateTime('rdv')->nullable();
