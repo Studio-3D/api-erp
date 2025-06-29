df655f09e8a8a3bec66b1f520a067eaea0e27063
airout <a.airout@gmail.com>
2025-06-29 14:44:30 +0100

    fixing test issues

--- app/Http/Requests/UpdateTypeBienRequest.php
+++ app/Http/Requests/UpdateTypeBienRequest.php
@@ -42,7 +42,7 @@ class UpdateTypeBienRequest extends FormRequest
     public function messages(): array
     {
         return [
-            'type.unique' => 'Ce type de bien est deja exist dans ce projet',
+            'type.unique' => 'Ce type de bien existe déjà dans ce projet',
         ];
     }
 }
