45239bd66a88d85af891e5652c259f668e52d50b
airout <a.airout@gmail.com>
2026-02-17 13:26:11 +0000

    Add logs folder creation in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -23,6 +23,11 @@ if [ ! -f storage/oauth-private.key ]; then
     php artisan passport:keys --force
 fi
 
+# Créer le dossier logs s'il n'existe pas
+mkdir -p storage/logs
+chown -R www-data:www-data storage
+chmod -R 775 storage
+
 # Permissions runtime
 chown -R www-data:www-data storage bootstrap/cache
 
